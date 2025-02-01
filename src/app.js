ce157c9cac30609ff22c477a0d67dd6897d792a3
Білий Олександр <71928700+AlexWhiteoff@users.noreply.github.com>
2025-02-01 22:26:49 +0200

    Add color mode toggle and update views with Handlebars templates

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const dotenv = require("dotenv");
 const express = require("express");
+const hbs = require("hbs");
+const path = require("path");
 
 const app = express();
 dotenv.config();
@@ -7,6 +9,16 @@ dotenv.config();
 const port = process.env.PORT;
 
 app.set("view engine", "hbs");
+
+hbs.registerPartials(path.join(__dirname + "/../views/partials"));
+
+app.use(express.static(path.join(__dirname + "/../public")));
+
+app.get("/", (req, res) => {
+    res.render("index.hbs", {
+        application: { pageTitle: "Weather App", name: "Weather App" },
+    });
+});
 
 app.listen(port, () => {
     console.log(`Weather app (http://localhost:${port}) is listening on port ${port}`);
